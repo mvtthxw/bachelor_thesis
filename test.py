db22a997d643f8b9434c74abc9dc8f7d5eedf4bd
241423 <241423@student.pwr.edu.pl>
2020-10-02 18:38:49 +0200

    add test

---
+++ test.py
@@ -0,0 +1,15 @@
+import nmap
+import sys
+nm=nmap.PortScanner()
+#print(sys.argv[1])
+address='127.0.0.1'
+range='1-443'
+type=sys.argv[1]
+print(type)
+nm.scan(address, range, type)
+
+for host in nm.all_hosts():  # lista przeskanowanych hostów
+    for protocol in nm[host].all_protocols():  # lista protokołów wykrytych podczas skanowania
+        port_list = nm[host][protocol].keys()  # lista portów w danym protokole
+        for port in port_list:
+            print(port)
